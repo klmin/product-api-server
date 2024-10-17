4164c1c1f257564a2e59cf2bf1a6dedebff327db
klmin <haemmas@naver.com>
2024-10-17 22:59:59 +0900

    fix(validation): Remove duplicate @NotBlank annotation in UserCreateRequest

--- src/main/java/com/product/user/request/UserCreateRequest.java
+++ src/main/java/com/product/user/request/UserCreateRequest.java
@@ -15,11 +15,9 @@ public record UserCreateRequest
     String loginId,
 
     @NotBlank
-    @NotBlank
     @Size(min=3, max=50)
     String userName,
 
-    @NotBlank
     @NotBlank
     @Pattern(regexp = PatternUtil.PASSWORD_REGEX, message="비밀번호는 8자 이상 20자 이하, 하나의 대문자, 소문자, 숫자, 특수문자를 포함해야 합니다.")
     String password,
