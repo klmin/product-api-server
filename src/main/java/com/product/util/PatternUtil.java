a83074cbedde791342b4d869b1ef8a5553db82a1
klmin <haemmas@naver.com>
2024-10-17 22:53:47 +0900

    refactor(PatternUtil): Add ID_REGEX in PatternUtil

--- src/main/java/com/product/util/PatternUtil.java
+++ src/main/java/com/product/util/PatternUtil.java
@@ -5,6 +5,7 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class PatternUtil {
+    public static final String ID_REGEX = "^[a-zA-Z0-9]{5,20}$";
     public static final String PHONE_NUMBER_REGEX = "^010\\d{8}$";
     public static final String EMAIL_REGEX = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
     public static final String PASSWORD_REGEX = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&#*])[A-Za-z\\d@$!%*?&#*]{8,20}$";
