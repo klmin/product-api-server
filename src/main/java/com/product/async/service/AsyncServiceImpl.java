af983ca415c7e59f681a57990327fe5fdda208a0
klmin <haemmas@naver.com>
2024-10-18 01:04:55 +0900

    feat(logging): Add P6Spy configuration and add @Transanctional AsyncService

--- src/main/java/com/product/async/service/AsyncServiceImpl.java
+++ src/main/java/com/product/async/service/AsyncServiceImpl.java
@@ -7,10 +7,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class AsyncServiceImpl implements AsyncService {
 
     private final ApiLogService apiLogService;
