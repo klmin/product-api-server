1c7bd8d8846584a12132c4845d270691a7e695f9
klmin <haemmas@naver.com>
2024-10-27 23:23:41 +0900

    feat: add delete method and refine exception handling to specific locations

--- src/test/java/com/product/config/abstracts/AbstractMvcTest.java
+++ src/test/java/com/product/config/abstracts/AbstractMvcTest.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.product.api.exception.handler.ApiExceptionHandler;
 import com.product.messagesource.service.MessageSourceService;
 import com.product.objectmapper.util.ObjectMapperUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,12 +14,14 @@ import org.springframework.http.converter.json.MappingJackson2HttpMessageConvert
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
+import java.io.UnsupportedEncodingException;
 import java.util.Map;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -26,6 +29,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Slf4j
 public abstract class AbstractMvcTest {
 
     @Autowired
@@ -48,7 +52,7 @@ public abstract class AbstractMvcTest {
                 .build();
     }
 
-    public MvcResult performRequestJsonAndResultMatcher(MockHttpServletRequestBuilder requestBuilder, String content, MultiValueMap<String, String> params, ResultMatcher... matchers) throws Exception {
+    public MvcResult performRequestJsonAndResultMatcher(MockHttpServletRequestBuilder requestBuilder, String content, MultiValueMap<String, String> params, ResultMatcher... matchers)  {
 
         requestBuilder.contentType(MediaType.APPLICATION_JSON);
 
@@ -59,61 +63,75 @@ public abstract class AbstractMvcTest {
             requestBuilder.params(params);
         }
 
-        var resultActions = this.mockMvc.perform(requestBuilder);
+        ResultActions resultActions = null;
 
-        for (ResultMatcher matcher : matchers) {
-            resultActions.andExpect(matcher);
+        try {
+            resultActions = this.mockMvc.perform(requestBuilder);
+
+            for (ResultMatcher matcher : matchers) {
+                resultActions.andExpect(matcher);
+            }
+        }catch(Exception e){
+            throw new RuntimeException(e);
         }
 
         return resultActions.andReturn();
     }
 
-    public MvcResult performRequestJson(MockHttpServletRequestBuilder requestBuilder, String content, MultiValueMap<String, String> params, ResultMatcher... matchers) throws Exception {
+    public MvcResult performRequestJson(MockHttpServletRequestBuilder requestBuilder, String content, MultiValueMap<String, String> params, ResultMatcher... matchers) {
         return performRequestJsonAndResultMatcher(requestBuilder, content, params, matchers);
     }
 
-    public MvcResult mvcResult(MockHttpServletRequestBuilder requestBuilder, String content, MultiValueMap<String, String> params, ResultMatcher... matchers) throws Exception {
+    public MvcResult mvcResult(MockHttpServletRequestBuilder requestBuilder, String content, MultiValueMap<String, String> params, ResultMatcher... matchers) {
         return performRequestJson(requestBuilder, content, params, matchers);
     }
 
-    public MockHttpServletResponse mvcResultResponse(MockHttpServletRequestBuilder requestBuilder, String content, MultiValueMap<String, String> params, ResultMatcher... matchers) throws Exception {
+    public MockHttpServletResponse mvcResultResponse(MockHttpServletRequestBuilder requestBuilder, String content, MultiValueMap<String, String> params, ResultMatcher... matchers) {
         return mvcResult(requestBuilder, content, params, matchers).getResponse();
     }
 
-    public String contentAsString(MockHttpServletRequestBuilder requestBuilder, String content, MultiValueMap<String, String> params, ResultMatcher... matchers) throws Exception {
-        return mvcResultResponse(requestBuilder, content, params, matchers).getContentAsString();
+    public String contentAsString(MockHttpServletRequestBuilder requestBuilder, String content, MultiValueMap<String, String> params, ResultMatcher... matchers) {
+        try{
+            return mvcResultResponse(requestBuilder, content, params, matchers).getContentAsString();
+        }catch(UnsupportedEncodingException e){
+            throw new RuntimeException(e);
+        }
     }
 
-    public <T> T response(MockHttpServletRequestBuilder requestBuilder, String content, MultiValueMap<String, String> params, Class<T> clazz, ResultMatcher... matchers) throws Exception {
+    public <T> T response(MockHttpServletRequestBuilder requestBuilder, String content, MultiValueMap<String, String> params, Class<T> clazz, ResultMatcher... matchers)  {
         return objectMapperUtil.readValue(contentAsString(requestBuilder, content, params, matchers), clazz);
     }
 
-    public <T> T response(MockHttpServletRequestBuilder requestBuilder, String content, MultiValueMap<String, String> params, TypeReference<T> clazz, ResultMatcher... matchers) throws Exception {
+    public <T> T response(MockHttpServletRequestBuilder requestBuilder, String content, MultiValueMap<String, String> params, TypeReference<T> clazz, ResultMatcher... matchers)  {
         return objectMapperUtil.readValue(contentAsString(requestBuilder, content, params, matchers), clazz);
     }
 
-    public <T> T getForObject(String url, Object obj, Class<T> clazz, ResultMatcher... matchers) throws Exception{
+    public <T> T getForObject(String url, Object obj, Class<T> clazz, ResultMatcher... matchers){
         return response(get(url), null, multiValueMap(obj), clazz, matchers);
     }
 
-    public <T> T postForObject(String url, Object obj, Class<T> clazz, ResultMatcher... matchers) throws Exception{
+    public <T> T postForObject(String url, Object obj, Class<T> clazz, ResultMatcher... matchers) {
         return response(post(url), objectMapperUtil.writeValueAsString(obj), null, clazz, matchers);
     }
 
-    public <T> T patchForObject(String url, Object obj, Class<T> clazz, ResultMatcher... matchers) throws Exception{
+    public <T> T patchForObject(String url, Object obj, Class<T> clazz, ResultMatcher... matchers) {
         return response(patch(url), objectMapperUtil.writeValueAsString(obj), null, clazz, matchers);
     }
 
-    public <T> T getForObject(String url, Object obj, TypeReference<T> clazz, ResultMatcher... matchers) throws Exception{
+    public <T> T getForObject(String url, Object obj, TypeReference<T> clazz, ResultMatcher... matchers){
         return response(get(url), null, multiValueMap(obj), clazz, matchers);
     }
 
-    public <T> T postForObject(String url, Object obj, TypeReference<T> clazz, ResultMatcher... matchers) throws Exception{
+    public <T> T postForObject(String url, Object obj, TypeReference<T> clazz, ResultMatcher... matchers) {
         return response(post(url), objectMapperUtil.writeValueAsString(obj), null, clazz, matchers);
     }
 
-    public <T> T patchForObject(String url, Object obj, TypeReference<T> clazz, ResultMatcher... matchers) throws Exception{
+    public <T> T patchForObject(String url, Object obj, TypeReference<T> clazz, ResultMatcher... matchers) {
         return response(patch(url), objectMapperUtil.writeValueAsString(obj), null, clazz, matchers);
+    }
+
+    public <T> T deleteById(String url, Object id, TypeReference<T> clazz, ResultMatcher... matchers){
+        return response(delete(url, id), null, null, clazz, matchers);
     }
 
     public <T> MultiValueMap<String, String> multiValueMap(T obj){
