1c758ac2b8c035dd9ae4182f88d8c633023674d7
klmin <haemmas@naver.com>
2024-10-29 14:38:12 +0900

    feat(welcome): Add WelComeController And hello.html

---
+++ src/main/java/com/product/welcome/WelComeController.java
@@ -0,0 +1,16 @@
+package com.product.welcome;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/welcome")
+public class WelComeController {
+
+    @GetMapping
+    public ResponseEntity<String> welcome() {
+        return ResponseEntity.ok("Welcome!");
+    }
+}
